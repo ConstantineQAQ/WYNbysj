f0de5bc05f790c28e0a10b1f40b231d8508d2c1c
wangyaning33 <wangyaning33@jd.com>
2024-02-02 10:19:17 +0800

    feat(SpringSecurity): 修改SpringSecurity验证方法

--- backend/base/src/main/java/com/constantineqaq/base/filter/JwtAuthenticationTokenFilter.java
+++ backend/base/src/main/java/com/constantineqaq/base/filter/JwtAuthenticationTokenFilter.java
@@ -29,32 +29,37 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         //我们先拿到请求头中的token
-        String token = request.getHeader("token");
-        if (StringUtils.isBlank(token)) {
-            //说明没有携带token 那么直接放行 之后的过滤器肯定会报错，那么就说明用户没有登录
-            filterChain.doFilter(request, response);
-            return;
+        //首先从Header中取出JWT
+        String authorization = request.getHeader("Authorization");
+        //判断是否包含JWT且格式正确
+        if (authorization != null && authorization.startsWith("Bearer ")) {
+            String token = authorization.substring(7);
+            if (StringUtils.isBlank(token)) {
+                //说明没有携带token 那么直接放行 之后的过滤器肯定会报错，那么就说明用户没有登录
+                filterChain.doFilter(request, response);
+                return;
+            }
+            //解析token
+            String userid;
+            try {
+                Claims claims = JwtUtils.parseJWT(token);
+                userid = claims.getSubject();
+            } catch (Exception e) {
+                log.error("解析token失败", e);
+                //就说明token失效了 或者是token无效
+                throw new RuntimeException("token无效");
+            }
+            //从redis中拿到用户的信息，给SecurityContextHolder设置上下文
+            LoginUser loginUser = (LoginUser) redisUtils.get("Login:" + userid);
+            if (Objects.isNull(loginUser)) {
+                throw new RuntimeException("用户未登录");
+            }
+            //存入SecurityContextHolder上下文当中  注意 这里必须得使用三个参数的authentication
+            //第三个参数为授权 也就是用户是啥身份 先不管
+            Authentication authentication = new UsernamePasswordAuthenticationToken(loginUser, null, loginUser.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            //放行
+            filterChain.doFilter(request, response); //那么就正常的请求接口去啦！！！
         }
-        //解析token
-        String userid;
-        try {
-            Claims claims = JwtUtils.parseJWT(token);
-            userid = claims.getSubject();
-        } catch (Exception e) {
-            log.error("解析token失败", e);
-            //就说明token失效了 或者是token无效
-            throw new RuntimeException("token无效");
-        }
-        //从redis中拿到用户的信息，给SecurityContextHolder设置上下文
-        LoginUser loginUser = (LoginUser) redisUtils.get("Login:" + userid);
-        if (Objects.isNull(loginUser)) {
-            throw new RuntimeException("用户未登录");
-        }
-        //存入SecurityContextHolder上下文当中  注意 这里必须得使用三个参数的authentication
-        //第三个参数为授权 也就是用户是啥身份 先不管
-        Authentication authentication = new UsernamePasswordAuthenticationToken(loginUser, null, loginUser.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        //放行
-        filterChain.doFilter(request, response); //那么就正常的请求接口去啦！！！
     }
 }
